805ff2d1e5660d8a98eb9c5b063b4ecef72e8f64
Paulo Moreira <prmoreira98@gmail.com>
2021-03-16 02:59:05 -0300

    declaring manager for user model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 
 from django.conf import settings
 from django.db.models.signals import post_save
@@ -14,6 +14,8 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
+
+    objects = UserManager()
 
 
 class Author(models.Model):
