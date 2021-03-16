031a160c6f9ffed276b6f843f1e3fddc7069797c
Paulo Moreira <prmoreira98@gmail.com>
2021-03-16 02:59:15 -0300

    login api url

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.authtoken import views as drf_views
 
 from . import views
 
 urlpatterns = [
         path('sign-up/', views.signup, name='signup'),
+        path('login/', drf_views.obtain_auth_token, name='login'),
 ]
