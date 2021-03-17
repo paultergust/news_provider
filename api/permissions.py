03892a665a657d38dba60dec88cc87869b70e7a9
Paulo Moreira <prmoreira98@gmail.com>
2021-03-17 00:44:13 -0300

    custom permission class

---
+++ api/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+from rest_framework.authtoken.models import Token
+
+from api.models import User
+
+class AdminAuthentication(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        try:
+            token = request.headers['token']
+        except:
+            token = ''
+
+        token_query = Token.objects.filter(key=request.headers['token']).first()
+        user = token_query.user
+        print(user.is_admin)
+        return user.is_admin
